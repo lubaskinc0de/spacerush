0dd72e8df78490f54acd80d91e6f3579cb691164
lubaskinc0de <lubaskincorporation@gmail.com>
2023-01-10 23:10:05 +0300

    Readme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from core.game import Game
 
 if __name__ == "__main__":
-    game = Game(600, 500, "Space rush!", 60, mobs_count=6, lives=3)
+    game = Game(1000, 800, "Space rush!", 60, mobs_count=10, lives=3)
 
     game.start()
